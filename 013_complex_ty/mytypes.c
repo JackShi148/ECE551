3a6badaba61e7e2542b3315843759adaedc60f2b
zs148 <zs148@duke.edu>
2022-09-05 16:15:55 -0400

    Did assignment 13

---
+++ 013_complex_ty/mytypes.c
@@ -0,0 +1,7 @@
+(1) struct _complex_num_t {
+  double real_part;
+  double imaginary_part;
+};
+typedef struct _complex_num_t complex_num_t;
+(2) enum _animal_t { CAT, FISH, SNAKE, DOG, HORSE };
+typedef enum _animal_t animal_t;
