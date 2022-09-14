3d6b8c5303c1db01e4b0ab937283b4d52cdf7571
zs148 <zs148@duke.edu>
2022-09-14 17:42:09 -0400

    Did assignment 28

---
+++ 028_tests_power/test-power.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned y);
+
+void check_power(unsigned x, unsigned y, unsigned expected_value) {
+  if (power(x, y) != expected_value) {
+    printf("when x=%u, y=%u, the answer is wrong", x, y);
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main(void) {
+  check_power(0, 0, 1);
+  check_power(4, 0, 1);
+  check_power(0, 6, 0);
+  check_power(2, 3, 8);
+  check_power(4290000000, 1, 4290000000);
+  check_power(1, 10, 1);
+  return EXIT_SUCCESS;
+}
