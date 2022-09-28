62075e6ad82f18f7b28ad0c44562d98e5a26760f
zs148 <zs148@duke.edu>
2022-09-28 19:26:51 -0400

    Did assignment 47

---
+++ 047_break_encr/breaker.c
@@ -0,0 +1,53 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void breaker(FILE * f) {
+  int charFreq[26];
+  int c;
+  for (int i = 0; i < 26; i++) {
+    charFreq[i] = 0;
+  }
+  while ((c = fgetc(f)) != EOF) {
+    if (isalpha(c)) {
+      c = tolower(c);
+      charFreq[c - 'a']++;
+    }
+    int suspect = 0;
+    int maxfreq = 0;
+    for (int i = 0; i < 26; i++) {
+      if (maxfreq < charFreq[i]) {
+        maxfreq = charFreq[i];
+        suspect = i;
+      }
+    }
+    if (maxfreq == 0) {
+      fprintf(stderr, "Not letter in the file\n");
+      exit(EXIT_FAILURE);
+    }
+    else if (4 <= suspect && suspect <= 25) {
+      fprintf(stdout, "%d\n", suspect - 4);
+    }
+    else if (0 <= suspect && suspect <= 3) {
+      fprintf(stdout, "%d\n", suspect + 22);
+    }
+  }
+}
+
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "the number of arguments is wrong\n");
+    return EXIT_FAILURE;
+  }
+  FILE * f = fopen(argv[1], "r");
+  if (f == NULL) {
+    perror("Could not open file\n");
+    return EXIT_FAILURE;
+  }
+  breaker(f);
+  if (fclose(f) != 0) {
+    perror("Failed to close the input file!\n");
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
