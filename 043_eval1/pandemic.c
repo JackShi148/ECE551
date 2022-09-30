1805a773263c06cda6d07ebe67af8a152785b42e
zs148 <zs148@duke.edu>
2022-09-30 15:59:29 -0400

    Did step 1

---
+++ 043_eval1/pandemic.c
@@ -0,0 +1,56 @@
+#include "pandemic.h"
+
+#include <stdio.h>
+#include <string.h>
+
+country_t parseLine(char * line) {
+  //WRITE ME
+  char * commaPos = strchr(line, ',');
+  if (*commaPos == '\0') {
+    printf("invalid input: no comma\n");
+    exit(EXIT_FAILURE);
+  }
+  country_t ans;
+  ans.name[0] = '\0';
+  ans.population = 0;
+  int i = 0;
+  while (line[i] != ',' && i < 64) {
+    ans.name[i] = line[i];
+    i++;
+  }
+  if (i == 64) {
+    printf("invalid input: the name is too long\n");
+    exit(EXIT_FAILURE);
+  }
+  else {
+    ans.name[i] = '\0';
+    i++;
+  }
+  if (line[i] == '\0') {
+    printf("invalid input: no population\n");
+    exit(EXIT_FAILURE);
+  }
+  char * population = &line[i];
+  char * end;
+  ans.population = strtol(population, &end, 10);
+  /*if (*end != '\0' && strcmp(population, "Population") != 0) {
+    printf("invalid input: invalid population\n");
+    exit(EXIT_FAILURE);
+  }*/
+  return ans;
+}
+
+void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
+  //WRITE ME
+}
+
+void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
+  //WRITE ME
+}
+
+void printCountryWithMax(country_t * countries,
+                         size_t n_countries,
+                         unsigned ** data,
+                         size_t n_days) {
+  //WRITE ME
+}
