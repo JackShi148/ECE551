a437abf0f72c95f5bfe5051ff4f88ee4dbe24b80
zs148 <zs148@duke.edu>
2022-10-04 15:40:29 -0400

    update code and testcases

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -60,8 +60,7 @@ void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
     exit(EXIT_FAILURE);
   }
   if (n_days < 7) {
-    fprintf(stderr, "invalid parameter: the number of day must not less than 7\n");
-    exit(EXIT_FAILURE);
+    exit(EXIT_SUCCESS);
   }
   for (size_t i = 0; i < n_days - 6; i++) {
     double sum = 0.0;
