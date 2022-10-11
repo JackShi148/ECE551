4b1ebaf6bc3666c614c9a052ea971e0fbcaaafe5
zs148 <zs148@duke.edu>
2022-10-11 11:30:23 -0400

    Did assignment 57

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,17 @@
+#include "outname.h"
+
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char * suffix = ".counts";
+  size_t len = strlen(inputName) + strlen(suffix) + 1;
+  char * outputName = strdup(inputName);
+  outputName = realloc(outputName, len * sizeof(*outputName));
+  assert(outputName != NULL);
+  outputName = strcat(outputName, suffix);
+  return outputName;
 }
