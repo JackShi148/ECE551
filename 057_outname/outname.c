3990c2fbff12761c8db1f22f7d66a665d8cb3849
zs148 <zs148@duke.edu>
2022-10-11 11:41:07 -0400

    Did assignment 57

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -9,9 +9,9 @@ char * computeOutputFileName(const char * inputName) {
   //WRITE ME
   char * suffix = ".counts";
   size_t len = strlen(inputName) + strlen(suffix) + 1;
-  char * outputName = strdup(inputName);
-  outputName = realloc(outputName, len * sizeof(*outputName));
+  char * outputName = malloc(len * sizeof(*outputName));
   assert(outputName != NULL);
+  outputName = strcpy(outputName, inputName);
   outputName = strcat(outputName, suffix);
   return outputName;
 }
