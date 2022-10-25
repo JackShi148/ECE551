f09d668b384d9422011cb9ba8ef9264648c652f1
zs148 <zs148@duke.edu>
2022-10-25 10:07:20 -0400

    update testcases

--- 074_int_array/IntArray.cpp
+++ 074_int_array/IntArray.cpp
@@ -1,22 +1,36 @@
 #include "IntArray.h"
+
 #include <assert.h>
+
 #include <ostream>
 
-IntArray::IntArray() {
-
+IntArray::IntArray() : data(NULL), numElements(0) {
 }
-IntArray::IntArray(int n) {
-
+IntArray::IntArray(int n) : data(new int[n]), numElements(n) {
 }
 
-IntArray::IntArray(const IntArray & rhs)  {
-
+IntArray::IntArray(const IntArray & rhs) {
+  numElements = rhs.numElements;
+  data = new int[numElements];
+  for (int i = 0; i < numElements; i++) {
+    data[i] = rhs.data[i];
+  }
 }
 IntArray::~IntArray() {
-
+  delete[] data;
 }
 
 IntArray & IntArray::operator=(const IntArray & rhs) {
+  if (this != &rhs) {
+    numElements = rhs.numElements;
+    int * temp = new int[rhs.numElements];
+    for (int i = 0; i < rhs.numElements; i++) {
+      temp[i] = rhs.data[i];
+    }
+    delete[] data;
+    data = temp;
+  }
+  return *this;
 }
 const int & IntArray::operator[](int index) const {
 }
@@ -33,5 +47,4 @@ bool IntArray::operator!=(const IntArray & rhs) const {
 }
 
 std::ostream & operator<<(std::ostream & s, const IntArray & rhs) {
-
 }
