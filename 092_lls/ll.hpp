2a33ad2681e518a86845e0bf37479810280846e9
zs148 <zs148@duke.edu>
2022-11-08 13:32:41 -0500

    Did assignment 92

--- 092_lls/ll.hpp
+++ 092_lls/ll.hpp
@@ -5,7 +5,168 @@
 
 #include <cstdlib>
 #include <exception>
+#include <iostream>
 
-//YOUR CODE GOES HERE
+class Tester;
+
+class not_exist : public std::exception {
+ private:
+  const char * message;
+
+ public:
+  not_exist() : message("the requested item does not exist") {}
+  virtual const char * what() const throw() { return message; }
+};
+
+template<typename T>
+class LinkedList {
+ private:
+  class Node {
+   public:
+    T data;
+    Node * next;
+    Node * prev;
+    Node(const T & d) : data(d), next(NULL), prev(NULL) {}
+    Node(const T & d, Node * n, Node * p) : data(d), next(n), prev(p){};
+  };
+  int size;
+  Node * head;
+  Node * tail;
+  bool removeItem(const T & item) {
+    bool flag = false;
+    Node ** current = &head;
+    while (*current != NULL && (*current)->data != item) {
+      current = &(*current)->next;
+    }
+    if ((*current) != NULL && (*current)->data == item) {
+      flag = true;
+      Node * temp = *current;
+      *current = (*current)->next;
+      if (*current != NULL) {
+        (*current)->prev = temp->prev;
+      }
+      else {
+        tail = temp->prev;
+      }
+      delete temp;
+      size--;
+    }
+    return flag;
+  }
+
+ public:
+  LinkedList() : size(0), head(NULL), tail(NULL){};
+  LinkedList(const LinkedList & rhs) : size(0), head(NULL), tail(NULL) {
+    for (int i = 0; i < rhs.getSize(); i++) {
+      addBack(rhs[i]);
+    }
+  }
+  LinkedList & operator=(const LinkedList & rhs) {
+    LinkedList newll;
+    for (int i = 0; i < rhs.getSize(); i++) {
+      newll.addBack(rhs[i]);
+    }
+    Node * temp = NULL;
+    while (head != NULL) {
+      temp = head;
+      head = head->next;
+      delete temp;
+    }
+    tail = NULL;
+    head = newll.head;
+    tail = newll.tail;
+    size = rhs.getSize();
+    newll.head = NULL;
+    newll.tail = NULL;
+    return (*this);
+  }
+  ~LinkedList() {
+    Node * temp = NULL;
+    while (head != NULL) {
+      temp = head;
+      head = head->next;
+      delete temp;
+    }
+    head = NULL;
+    tail = NULL;
+  }
+  int getSize() const { return this->size; }
+  void addFront(const T & item) {
+    head = new Node(item, head, NULL);
+    if (head->next != NULL) {
+      head->next->prev = head;
+    }
+    if (tail == NULL) {
+      tail = head;
+    }
+    size++;
+  }
+  void addBack(const T & item) {
+    tail = new Node(item, NULL, tail);
+    if (tail->prev != NULL) {
+      tail->prev->next = tail;
+    }
+    if (head == NULL) {
+      head = tail;
+    }
+    size++;
+  }
+  bool remove(const T & item) {
+    bool flag = false;
+    flag = removeItem(item);
+    return flag;
+  }
+  T & operator[](int index) {
+    try {
+      if (index < 0 || index >= size) {
+        throw(not_exist());
+      }
+    }
+    catch (not_exist & e) {
+      std::cerr << e.what() << std::endl;
+    }
+    Node * cur = head;
+    for (int i = 0; i < size; i++) {
+      if (i == index) {
+        break;
+      }
+      else {
+        cur = cur->next;
+      }
+    }
+    return cur->data;
+  }
+  const T & operator[](int index) const {
+    try {
+      if (index < 0 || index >= size) {
+        throw(not_exist());
+      }
+    }
+    catch (not_exist & e) {
+      std::cerr << e.what() << std::endl;
+    }
+    Node * cur = head;
+    for (int i = 0; i < size; i++) {
+      if (i == index) {
+        break;
+      }
+      else {
+        cur = cur->next;
+      }
+    }
+    return cur->data;
+  }
+  int find(const T & item) const {
+    int index = -1;
+    for (int i = 0; i < size; i++) {
+      if (item == (*this)[i]) {
+        index = i;
+        break;
+      }
+    }
+    return index;
+  }
+  friend class Tester;
+};
 
 #endif
---
+++ 092_lls/test-lls.cpp
@@ -0,0 +1,84 @@
+#include <assert.h>
+
+#include <cstdlib>
+#include <iostream>
+
+#include "ll.hpp"
+
+class Tester {
+ public:
+  // testing for default constructor is done for you
+  void testDefCtor() {
+    LinkedList<int> il;
+    assert(il.head == NULL);
+    assert(il.tail == NULL);
+    assert(il.getSize() == 0);
+  }
+  // example of another method you might want to write
+  void testAdd() {
+    LinkedList<int> il;
+    il.addFront(1);  //[1]
+    assert(il.getSize() == 1 && il.head != NULL && il.tail == il.head &&
+           il.head->data == 1 && il.head->next == NULL && il.head->prev == NULL);
+    il.addFront(2);  //[2,1]
+    assert(il.getSize() == 2 && il.head != NULL && il.head->next != NULL &&
+           il.tail == il.head->next && il.head->data == 2 && il.head->prev == NULL &&
+           il.head->next->prev == il.head && il.head->next->next == NULL);
+    il.addBack(3);  //[2,1,3]
+    assert(il.getSize() == 3 && il.head->data == 2 && il.head->next->next->data == 3 &&
+           il.head->next->data == 1 && il.tail->data == 3 && il.tail->next == NULL &&
+           il.head->next == il.tail->prev && il.tail->prev->prev == il.head);
+  }
+  void testRuleofThree() {
+    LinkedList<int> list1;
+    list1.addFront(1);
+    list1.addBack(2);  //[1,2]
+    LinkedList<int> list2(list1);
+    assert(list2.getSize() == 2 && list2.head->data == 1 && list2.head->next->data == 2 &&
+           list2.head->next == list2.tail && list2.tail->prev == list2.head &&
+           list2.head->prev == NULL && list2.head->prev == list2.tail->next);
+    list1.remove(2);  //[1]
+    list2 = list1;    //[1]
+    assert(list2.getSize() == 1 && list2.head->data == 1 && list2.head == list2.tail &&
+           list2.head->next == NULL && list2.head->next == list2.head->prev);
+  }
+  void testRemoval() {
+    LinkedList<int> il;
+    il.addFront(1);
+    il.addBack(2);  //[1,2]
+    bool flag = false;
+    flag = il.remove(2);  //[1]
+    assert(flag == true && il.getSize() == 1 && il.head->data == 1 &&
+           il.tail == il.head && il.head->next == NULL && il.head->next == il.head->prev);
+    flag = false;
+    il.addFront(2);       //[2,1]
+    flag = il.remove(2);  //[1]
+    assert(flag == true && il.getSize() == 1 && il.head->data == 1 &&
+           il.head == il.tail && il.head->next == NULL && il.head->next == il.head->prev);
+    flag = il.remove(2);  //[1]
+    assert(flag == false && il.getSize() == 1 && il.head->data == 1 &&
+           il.head == il.tail && il.head->next == NULL && il.head->next == il.head->prev);
+    il.addFront(1);       //[1,1]
+    flag = il.remove(1);  //[1]
+    assert(flag == true && il.getSize() == 1 && il.head->data == 1 &&
+           il.head == il.tail && il.head->next == NULL && il.head->next == il.tail->prev);
+    flag = il.remove(1);  //[]
+    assert(flag == true && il.getSize() == 0 && il.head == NULL && il.head == il.tail);
+    il.addFront(1);
+    il.addFront(3);
+    il.addFront(2);       //[2,3,1]
+    flag = il.remove(3);  //[2,1]
+    assert(flag == true && il.getSize() == 2 && il.head->data == 2 &&
+           il.tail->data == 1 && il.head->next == il.tail && il.tail->prev == il.head &&
+           il.head->prev == NULL && il.head->prev == il.tail->next);
+  }
+};
+
+int main(void) {
+  Tester t;
+  t.testDefCtor();
+  t.testAdd();
+  t.testRemoval();
+  t.testRuleofThree();
+  return EXIT_SUCCESS;
+}
