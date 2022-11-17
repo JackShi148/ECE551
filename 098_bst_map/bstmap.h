3ace93037e67b6ff4673b06d6a01d26cf4e6f711
zs148 <zs148@duke.edu>
2022-11-17 08:24:11 -0500

    Did assignment 98

--- 098_bst_map/bstmap.h
+++ 098_bst_map/bstmap.h
@@ -46,43 +46,44 @@ class BstMap : public Map<K, V> {
   virtual void add(const K & key, const V & value) {
     this->root = addHelper(this->root, key, value);
   }
-  Node * addHelper(Node * current, const K & key, const V & value) {
-    if (current == NULL) {
+  Node * addHelper(Node * cur, const K & key, const V & value) {
+    if (cur == NULL) {
       Node * ans = new Node(key, value);
       return ans;
     }
     else {
-      if (key == current->key) {
-        current->value = value;
-        return current;
+      if (key == cur->key) {
+        cur->value = value;
+        return cur;
       }
-      else if (key < current->key) {
-        current->left = addHelper(current->left, key, value);
+      else if (key < cur->key) {
+        cur->left = addHelper(cur->left, key, value);
       }
       else {
-        current->right = addHelper(current->right, key, value);
+        cur->right = addHelper(cur->right, key, value);
       }
-      return current;
+      return cur;
     }
   }
 
   virtual const V & lookup(const K & key) const throw(std::invalid_argument) {
-    Node * cur = root;
-    while (cur != NULL) {
-      if (cur->key == key) {
+    Node * current = root;
+    while (current != NULL) {
+      if (key == current->key) {
         break;
       }
-      else if (key < cur->key) {
-        cur = cur->left;
+      else if (key < current->key) {
+        current = current->left;
       }
       else {
-        cur = cur->right;
+        current = current->right;
       }
     }
-    if (cur == NULL) {
-      throw std::invalid_argument("not find corresponding key");
+
+    if (current == NULL) {
+      throw std::invalid_argument("key not find\n");
     }
-    return cur->value;
+    return current->value;
   }
   virtual void remove(const K & key) {
     Node ** cur = &root;
