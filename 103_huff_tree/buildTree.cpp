1673f2a66e770495cd499c0be6bacadde1523659
zs148 <zs148@duke.edu>
2022-11-21 20:12:47 -0500

    did assigment 103

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,21 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  for (int i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while (pq.size() != 1) {
+    Node * cur_node_1 = pq.top();
+    pq.pop();
+    Node * cur_node_2 = pq.top();
+    pq.pop();
+    pq.push(new Node(cur_node_1, cur_node_2));
+  }
+  Node * root = pq.top();
+  pq.pop();
+  return root;
+}
